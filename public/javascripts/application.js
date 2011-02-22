aa371ecd586ccd94b75cb1f9bf6069713f14f851
Aaron Wright <awright418@yahoo.com>
2011-02-22 12:44:13 -0600

    Added page nav javascript.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,56 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+/* Gameroom Terminal */
+
+var gameroomlincoln_terminal = {
+  
+  run: function() {
+    
+    gameroomlincoln_terminal.init();
+    
+  },
+  
+  init: function() {
+    
+    /* Section Nav */
+    $('a.customer').click(function(event){
+      gameroomlincoln_terminal.showSection('customer');
+      event.preventDefault();
+    });
+    $('a.cart').click(function(event){
+      gameroomlincoln_terminal.showSection('cart');
+      event.preventDefault();
+    });
+    $('a.payment').click(function(event){
+      gameroomlincoln_terminal.showSection('payment');
+      event.preventDefault();
+    });
+    $('a.review').click(function(event){
+      gameroomlincoln_terminal.showSection('review');
+      event.preventDefault();
+    });
+    
+    /* Customer Nav */
+    $('a.customer_form').click(function(event){
+      gameroomlincoln_terminal.showCustomerSection('customer_form');
+      event.preventDefault();
+    });
+    $('a.customer_search_results').click(function(event){
+      gameroomlincoln_terminal.showCustomerSection('customer_search_results');
+      event.preventDefault();
+    });
+  },
+  
+  showSection: function(section) {
+    $('section').hide();
+    $('section#' + section).show();
+    $('ul#section_nav a').removeClass('active');
+    $('ul#section_nav a.' + section).addClass('active');
+  },
+  
+  showCustomerSection: function(section) {
+    $('div#customer_page_container > div').hide();
+    $('div#customer_page_container > div#' + section).show();
+    $('ul#customer_nav a').removeClass('active');
+    $('ul#customer_nav a.' + section).addClass('active');
+  }
+  
+};

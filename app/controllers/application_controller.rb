d946f4f2f2176e93a15b9cd7d0c2d5b81b485773
Aaron Wright <acwright@acwrightdesign.com>
2011-03-11 20:05:39 -0600

    Added layouts for reports and dashboard.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
       case request.format
       when Mime::JSON, Mime::XML
         authenticate_or_request_with_http_basic do |api_key, api_secret|
-          return false # This is broken! Should be authenticating!
+          return api_key == 'x' && api_secret == 'x'
         end
       end
     end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1978,7 +1978,6 @@ var TerminalController = new JS.Class({
 
     this.transaction_controller = new TransactionController();
     this.till_controller = new TillController('div#till');
-    this.user_nav = $('ul#user_nav').hide();
 
     this.till_controller.view.show();
     this.till_controller.addObserver(this.updateTill, this);
@@ -1991,7 +1990,6 @@ var TerminalController = new JS.Class({
   updateTill: function(till) {
     this.transaction_controller.newTransaction(till);
     $('li.current_user_till', this.user_nav).html(till.title);
-    $(this.user_nav).show();
     this.till_controller.view.hide();
   }
 });
